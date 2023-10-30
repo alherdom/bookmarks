04058075d4e80cc9051e3008bf4badd947865d00
Alejandro Hdez <alherdom@outlook.com>
2023-10-30 11:41:12 +0000

    Complete follow unfollow and activity stream

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "images.apps.ImagesConfig",
     "easy_thumbnails",
+    "actions.apps.ActionsConfig",
 ]
 
 MIDDLEWARE = [
--- images/views.py
+++ images/views.py
@@ -8,6 +8,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
 
 
 @login_required
@@ -20,6 +21,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == "like":
                 image.users_like.add(request.user)
+                create_action(request.user, "likes", image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({"status": "ok"})
@@ -40,6 +42,7 @@ def image_create(request):
             # assign current user to the item
             new_image.user = request.user
             new_image.save()
+            create_action(request.user, "bookmarked image", new_image)
             messages.success(request, "Image added successfully")
             # redirect to new created item detail view
             return redirect(new_image.get_absolute_url())
