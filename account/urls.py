4a3c265d146258a2a5b6357fc31d3f8631e04d97
Alejandro Hdez <alherdom@outlook.com>
2023-10-30 11:42:42 +0000

    Complete follow unfollow and activity stream

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path, include
-
 # from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from . import views
 
 
@@ -45,6 +44,6 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("edit/", views.edit, name="edit"),
     path("users/", views.user_list, name="user_list"),
-    path('users/follow/', views.user_follow, name='user_follow'),
+    path("users/follow/", views.user_follow, name="user_follow"),
     path("users/<username>/", views.user_detail, name="user_detail"),
 ]
